fdb8a104b8df2df9d26d80b548521848feb7bba5
BartekRutowicz <bartirut@gmail.com>
2024-11-21 19:07:19 +0100

    Search suggestions added

--- ArtHold/components/searchBar.vue
+++ ArtHold/components/searchBar.vue
@@ -3,30 +3,78 @@ import { ref, watch } from "vue";
 import { useRouter } from "vue-router";
 //import { useFilteredArtworksStore } from "~/stores/filteredArtworks";
 
-let input = ref("");
+const input = ref("");
 const router = useRouter();
+const searchHistory = ref<string[]>([]);
+const isActive = ref(false);
 //const store = useFilteredArtworksStore();
 
 const updateQueryInURL = useDebounceFn(() => {
   const query = input.value.trim();
-  router.push({ query: { query } });
+  if (query) {
+    router.push({ query: { query } });
+    saveQuery(query);
+  }
 }, 300);
+
+const saveQuery = useDebounceFn((query: string) => {
+  if (searchHistory.value.find((element) => element === query)) {
+    return;
+  }
+  if (searchHistory.value.length > 5) {
+    searchHistory.value.shift();
+  }
+  searchHistory.value.push(query);
+  localStorage.setItem("searchHistory", JSON.stringify(searchHistory.value));
+}, 600);
+
+const loadSearchHistory = () => {
+  const history = localStorage.getItem("searchHistory");
+  if (history) {
+    searchHistory.value = JSON.parse(history);
+  }
+};
+
+const inputFromHistory = (queryListing: string) => {
+  input.value = queryListing;
+  updateQueryInURL();
+};
+
+onMounted(() => {
+  loadSearchHistory();
+});
 </script>
 
 <template>
   <div class="relative">
     <input
+      v-model="input"
       @input="updateQueryInURL"
+      @focus="isActive = true"
+      @blur="isActive = false"
       class="bg-textSecondary text-black rounded-sm px-4 py-2 min-w-44 placeholder-gray-dark focus:outline-none text-primary focus:text-primary"
       type="text"
       placeholder="Search..."
-      v-model="input"
     />
     <NuxtImg
       src="/search-alt-2-svgrepo-com.svg"
       alt="Upload"
       class="w-6 h-max absolute right-3 top-1/2 transform -translate-y-1/2"
     />
+    <ul
+      v-if="isActive"
+      class="bg-textSecondary absolute mt-1 rounded-sm min-w-44"
+    >
+      <li
+        v-for="(query, index) in searchHistory.slice().reverse()"
+        :key="index"
+        @mousedown="inputFromHistory(query)"
+        @hover="isActive = true"
+        class="p-1 w-full hover:bg-textPrimary hover:text-textSecondary cursor-pointer"
+      >
+        {{ query }}
+      </li>
+    </ul>
   </div>
 </template>
 
